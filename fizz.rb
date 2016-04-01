251c49e3e9616255351a0104562f9b44ad5fc55c
Antonio Fernandez <eric6822@gmail.com>
2016-04-01 02:36:50 +0200

    First commit

---
+++ fizz.rb
@@ -0,0 +1,15 @@
+1.upto(100) do |i|
+	if i % 3 == 0 and i % 5 == 0
+		puts "FizzBuzz"
+	elsif i % 3 == 0
+		puts "Fizz"
+	elsif i % 5 == 0
+		puts "Buzz"
+	else 
+		puts i
+	end
+	if i.match(1)
+		puts " Bang"
+	end
+
+end
